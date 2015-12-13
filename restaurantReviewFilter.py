5805f4836ac4b55713aa436207ba21d95f7e74d9
arnkmish <arnkmish@indiana.edu>
2015-12-13 14:40:35 -0500

    added code for filtering restaurant reviews

---
+++ restaurantReviewFilter.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+	client = MongoClient()
+
+	dBase = client.yelp_test_recommend
+
+	review = dBase.yelp_test_review
+
+	restaurant = dBase.yelp_test_restaurant
+
+	restaurantReviews = dBase.yelp_test_res_review
+
+	restaurantReviews.remove({})
+
+	for item in review.find():
+		if restaurant.find_one({"business_id" : item["business_id"]}):
+				restaurantReviews.insert(item)
+
+	print "The total number of reviews for restaurants is : "
+	print restaurantReviews.count()
