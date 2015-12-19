c273a677e6c430147e4aa7e166320cf2bad5a822
arnkmish <arnkmish@indiana.edu>
2015-12-18 19:41:03 -0500

    updated the code with an initial documentation

--- allowedUserListBuilder.py
+++ allowedUserListBuilder.py
@@ -1,3 +1,10 @@
+'''
+
+This file prints out all the users for whom the system can provide recommendations. The output of this file can be redirected to a text file in order to store the user_id s of all these users, using the command '$python allowedUsrrListBuilder.py > allowedUserList.txt'.
+
+'''
+
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
